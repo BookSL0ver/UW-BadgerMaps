d55e5a51e4adccb86acd7fac5282da8b8cc5328b
BookSL0ver <jwindorff@wisc.edu>
2021-11-11 11:03:31 -0600

    Map working and putting a marker at user's initial location. Menu not working but started on.

--- app/src/main/java/com/example/uw_badgermaps/MapActivity.java
+++ app/src/main/java/com/example/uw_badgermaps/MapActivity.java
@@ -1,12 +1,15 @@
 package com.example.uw_badgermaps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -16,6 +19,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity {
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
     private FusedLocationProviderClient mFusedLocationProviderClient;
     private LatLng mLocationLatLng;
     private GoogleMap mMap;
@@ -34,8 +38,35 @@ public class MapActivity extends FragmentActivity {
 
     private void displayMyLocation() {
         int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION);
-        if (permission== PackageManager.PERMISSION_DENIED) {
+        if (permission == PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
+        else {
+            mFusedLocationProviderClient.getLastLocation().addOnCompleteListener(this, task -> {
+                Location mLastKnownLocation = task.getResult();
+                if (task.isSuccessful() && mLastKnownLocation != null) {
+                    mLocationLatLng = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(mLocationLatLng).title("Current Location"));
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantRestults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantRestults);
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantRestults.length > 0 && grantRestults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayMyLocation();
+            }
+        }
+    }
+
+    public void filters(View view) {
+        //TODO
+    }
+
+    public void search(View view) {
+        //TODO
     }
 }
