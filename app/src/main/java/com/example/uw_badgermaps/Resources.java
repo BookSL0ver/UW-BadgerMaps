b48e0b2ad763f7bea2e88670b8066ad7c623b8ad
BookSL0ver <jwindorff@wisc.edu>
2021-11-11 22:48:34 -0600

    minor fix

--- app/src/main/java/com/example/uw_badgermaps/Resources.java
+++ app/src/main/java/com/example/uw_badgermaps/Resources.java
@@ -3,6 +3,7 @@ package com.example.uw_badgermaps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Resources extends AppCompatActivity {
 
@@ -12,27 +13,27 @@ public class Resources extends AppCompatActivity {
         setContentView(R.layout.activity_resources);
     }
 
-    public void safeWalkClick() {
+    public void safeWalkClick(View view) {
         SafeWalkPopUp safeWalkPopUp = new SafeWalkPopUp();
         safeWalkPopUp.show(getSupportFragmentManager(), "Safe Walk");
     }
 
-    public void uhsClick() {
+    public void uhsClick(View view) {
         UHSPopUp uhsPopUp = new UHSPopUp();
         uhsPopUp.show(getSupportFragmentManager(), "UHS");
     }
 
-    public void librariesClick() {
+    public void librariesClick(View view) {
         LibrariesPopUp librariesPopUp = new LibrariesPopUp();
         librariesPopUp.show(getSupportFragmentManager(), "Libraries");
     }
 
-    public void techHelpClick() {
+    public void techHelpClick(View view) {
         TechHelpPopUp techHelpPopUp = new TechHelpPopUp();
         techHelpPopUp.show(getSupportFragmentManager(), "Tech Help");
     }
 
-    public void employmentClick() {
+    public void employmentClick(View view) {
         EmploymentPopUp employmentPopUp = new EmploymentPopUp();
         employmentPopUp.show(getSupportFragmentManager(), "Employment");
     }
