6bb35ebb7f6fc02d2c5307f7fe51e58b3c8d71bd
BookSL0ver <jwindorff@wisc.edu>
2021-11-22 00:25:15 -0600

    Settings pages layouts

---
+++ app/src/main/java/com/example/uw_badgermaps/Password.java
@@ -0,0 +1,14 @@
+package com.example.uw_badgermaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+    }
+}
