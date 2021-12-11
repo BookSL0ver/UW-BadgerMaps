36f22f518f0b2c4877eac326b5b573772b50851d
Josh Zogleman <jzogleman@wisc.edu>
2021-12-10 19:59:58 -0600

    updated search bar to take addresses

--- app/src/main/java/com/example/uw_badgermaps/MapActivity.java
+++ app/src/main/java/com/example/uw_badgermaps/MapActivity.java
@@ -7,9 +7,12 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SearchView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -19,6 +22,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapActivity extends FragmentActivity {
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
     private FusedLocationProviderClient mFusedLocationProviderClient;
@@ -26,6 +32,8 @@ public class MapActivity extends FragmentActivity {
     private GoogleMap mMap;
     //set to campus area for map zoom
     private final LatLng mDestinationLatLng = new LatLng(43.0720, -89.4076);
+    SearchView searchView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +46,34 @@ public class MapActivity extends FragmentActivity {
             //zoom into campus
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDestinationLatLng,17));
             displayMyLocation();
+            searchView = findViewById(R.id.svLocation);
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    String location = searchView.getQuery().toString();
+                    List<Address> addressList = null;
+
+                    if (location != null || !location.equals("")){
+                        Geocoder geocoder = new Geocoder(MapActivity.this);
+                        try{
+                            addressList = geocoder.getFromLocationName(location, 1);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Address address = addressList.get(0);
+                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
+
+                    }
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    return false;
+                }
+            });
         });
     }
 
