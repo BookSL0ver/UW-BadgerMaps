ca1b49b94dd4d7f37e31417ee9773e69dae38f11
BookSL0ver <jwindorff@wisc.edu>
2021-12-12 20:29:56 -0600

    committing so it lets me pull

--- app/src/main/java/com/example/uw_badgermaps/Settings.java
+++ app/src/main/java/com/example/uw_badgermaps/Settings.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
 
 public class Settings extends AppCompatActivity {
+    ToggleButton darkMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Spinner spinner = (Spinner) findViewById(R.id.gpsPrecSelect);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gps_precision, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        darkMode = findViewById(R.id.darkModeSwitch);
     }
 
     public void signUpClick(View view) {
@@ -27,5 +36,14 @@ public class Settings extends AppCompatActivity {
     public void accountClick(View view) {
         Intent intent = new Intent(this, Account.class);
         startActivity(intent);
+    }
+
+    public void toggle(View view) {
+        if(darkMode.isChecked()) {
+            //TODO
+        }
+        else {
+            //TODO
+        }
     }
 }
